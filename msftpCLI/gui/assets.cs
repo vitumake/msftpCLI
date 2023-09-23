96af1176e62c45182c8de473e16ae609f6926b1b
markus <mag3768@gmail.com>
2023-09-23 19:31:13 +0300

    initial

---
+++ msftpCLI/gui/assets.cs
@@ -0,0 +1,5 @@
+using Terminal.Gui;
+
+class Assets {
+    
+}
---
+++ msftpCLI/program.cs
@@ -0,0 +1,28 @@
+﻿using Terminal.Gui;
+
+class Gui {
+    static int Main ()
+    {
+        Application.Init ();
+        var menu = new MenuBar (new MenuBarItem [] {
+            new MenuBarItem ("_File", new MenuItem [] {
+                new MenuItem ("_Quit", "", () => { 
+                    Application.RequestStop (); 
+                })
+            }),
+        });
+
+        var win = new Window ("Hello") {
+            X = 0,
+            Y = 1,
+            Width = Dim.Fill (),
+            Height = Dim.Fill () - 1
+        };
+
+        // Add both menu and win in a single call
+        Application.Top.Add (menu, win);
+        Application.Run ();
+        Application.Shutdown ();
+        return 0;
+    }
+}
